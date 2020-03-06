1cfda31e7e41b156695b87e721fa2dd53cdbb343
Josef Stein Butzke <josef@MacBook-Pro-de-Josef.local>
2020-03-05 22:50:35 -0300

    Creation of database and config ORM

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,7 +1,8 @@
 // import jwt from 'jsonwebtoken';
-
+const sequelize = require('../../database/index')
 class SessionController {
     async store(req, res) {
+        sequelize;
         return res.json({message: req.body.message})
     }
 }
